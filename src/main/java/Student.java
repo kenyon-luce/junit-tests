d7057b5f529feb418675e7ae94a8a06817a77af4
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-14 14:07:14 -0500

    almost done with part 1, getGrades needs work

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class Student {
+    private String name;
+    private long id;
+    private ArrayList<Long> grades;
+
+    public Student(String name, long id){
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<Long>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public ArrayList<Long> getGrades(){
+        return this.grades;
+    }
+
+    public long getGradeAverage(){
+        long total = 0;
+        for(long grade : grades){
+            total += grade;
+        }
+
+        return total/grades.size();
+    }
+
+    public void addGrade(long grade){
+        grades.add(grade);
+    }
+
+
 }
