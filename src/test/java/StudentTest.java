5445910426d20a996933333e1927b0b9efcdd617
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-14 16:02:18 -0500

    updating part 1

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -5,7 +5,11 @@ public class StudentTest {
     @Test
     public void testStudent(){
         Student test = new Student("student", 1L);
+        //mentioned from instructor, its a good idea to test if the student even exists
+        Student nonExistent = null;
 
+        assertNotNull(test);
+        assertNull(nonExistent);
         assertEquals("student", test.getName());
         assertEquals(1L, test.getId());
     }
@@ -16,7 +20,7 @@ public class StudentTest {
         grade.addGrade(100);
         grade.addGrade(90);
 
-        assertSame(2, grade.getGrades().size());
+        assertEquals(2, grade.getGrades().size());
         assertSame(100, grade.getGrades().get(0));
         assertSame(90, grade.getGrades().get(1));
         assertEquals(95, grade.getGradeAverage());
