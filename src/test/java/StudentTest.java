05c5fb39b37f2c54aaf93dd9ac30da7f824ca086
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-14 13:52:15 -0500

    added tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,3 +1,23 @@
-public class StudentTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class StudentTest {
+    @Test
+    public void testStudent(){
+        Student test = new Student("student", 1L);
+
+        assertEquals("student", test.getName());
+        assertEquals(1L, test.getId());
+    }
+
+    @Test
+    public void testGrade(){
+        Student grade = new Student("grade", 2L);
+        grade.addGrade(100);
+        grade.addGrade(90);
+
+        assertEquals(100, grade.getGrades().get(0));
+        assertEquals(90, grade.getGrades().get(1));
+        assertEquals(95, grade.getGradeAverage());
+    }
 }
